66feb7486318a89793d26fde0a418031ccb61285
Luis Fernando Miranda <abraxas@Luiss-MacBook-Pro.local>
2022-08-19 23:44:22 -0500

    changing text: px to rem

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,15 +13,13 @@
 	<div id="wrapper" class="mx-[10px] sm:mx-[50px]  ">
 		<!--Main Banners-->
 		<div class="mt-[30px] snap-start ">
-			<h2
-				class=" font-semibold text-center mb-[15px] mx-auto text-white text-[10px] sm:text-[30px]  "
-			>
+			<h2 class=" font-semibold text-center mb-[15px] mx-auto text-white text-xs sm:text-4xl  ">
 				SATROCKS revient le 7 Octobre 2022 pour sa XIV edition
 			</h2>
 			<!--Banner-->
 			<MainBanner />
 
-			<p class="mt-[5px] text-center text-white text-[5px] sm:text-[19px]">
+			<p class="mt-[5px] text-center text-white text-[5px] sm:text-xl">
 				19h00 : Ouverture des portes - 20h00 : Début des concerts - 02h30 : Fin de la Musique -
 				03h00 : Fermeture des portes
 			</p>
@@ -42,7 +40,7 @@
 					aria-expanded="true"
 					aria-controls="accordion-color-body-1"
 				>
-					<span class="font-bold text-[25px] sm:text-[40px]">SCENE SAT</span>
+					<span class="font-bold text-2xl sm:text-5xl">SCENE SAT</span>
 				</button>
 			</h2>
 			<div id="accordion-color-body-1" class="" aria-labelledby="accordion-color-heading-1">
@@ -57,7 +55,7 @@
 					aria-expanded="false"
 					aria-controls="accordion-color-body-2"
 				>
-					<span class="font-bold text-[25px] sm:text-[40px]">SCENE UNDERGROUND</span>
+					<span class="font-bold text-2xl sm:text-5xl">SCENE UNDERGROUND</span>
 				</button>
 			</h2>
 			<div id="accordion-color-body-2" class="hidden" aria-labelledby="accordion-color-heading-2">
@@ -72,7 +70,7 @@
 					aria-expanded="false"
 					aria-controls="accordion-color-body-3"
 				>
-					<span class="font-bold text-[25px] sm:text-[40px]">SCENE CHILL</span>
+					<span class="font-bold text-2xl sm:text-5xl">SCENE CHILL</span>
 				</button>
 			</h2>
 			<div id="accordion-color-body-3" class="hidden" aria-labelledby="accordion-color-heading-3">
@@ -90,15 +88,15 @@
 			>
 				<TranslucidBox styles="sm:mx-0 sm:w-[700px] sm:h-[200px] h-[100px]">
 					<div class="sm:m-[40px] ">
-						<h1 class="font-bold text-white text-left text-[30px] sm:text-[45px] ">15.- CHF</h1>
-						<p class="text-white mt-[1px] sm:mt-[8px] sm:text-[20px]">Plein tarif</p>
+						<h1 class="font-bold text-white text-left text-3xl sm:text-5xl ">15.- CHF</h1>
+						<p class="text-white mt-[1px] sm:mt-[8px] sm:text-xl">Plein tarif</p>
 					</div>
 				</TranslucidBox>
 
 				<TranslucidBox styles="sm:mx-0 sm:w-[700px] sm:h-[200px] h-[100px]">
 					<div class="sm:m-[40px] ">
-						<h1 class="font-bold text-white text-left text-[30px] sm:text-[45px] ">10.- CHF</h1>
-						<p class="text-white mt-[1px] sm:mt-[8px] sm:text-[20px]">
+						<h1 class="font-bold text-white text-left text-3xl sm:text-5xl ">10.- CHF</h1>
+						<p class="text-white mt-[1px] sm:mt-[8px] sm:text-xl">
 							Sur présentation de la carte Sat à I’entrée
 						</p>
 					</div>
@@ -106,53 +104,67 @@
 			</div>
 
 			<div id="buyPlaces" class="mt-[10px] text-white sm:mt-[18px]">
-				<p>Vous pouvez acheter des places:</p>
-				<p>- en ligne, sur le site Petzi</p>
-				<p>- chez l'un des points de vente Petzi</p>
-				<p>- à Satellite entre 11h et 14h ou à semaine des associations jusqu'a soldout!</p>
+				<p>
+					Vous pouvez acheter des places:
+					<br />
+					- en ligne, sur le site Petzi
+					<br />
+					- chez l'un des points de vente Petzi
+					<br />
+					- à Satellite entre 11h et 14h ou à semaine des associations jusqu'a soldout!
+				</p>
 			</div>
 		</div>
 
 		<div id="Information" class="snap-start pb-5 mt-[30px] space-y-3 ">
 			<h1 class="font-bold text-[42px] sm:text-[90px] text-[#0A4979]">INFORMATION</h1>
 
-			<div class="sm:flex sm:flex-row sm:space-x-10 sm:pb-[500px]">
+			<div class="sm:flex sm:flex-row sm:space-x-10 sm:pb-[900px]">
 				<!-- fist 2 info -->
-				<div class="space-y-3 sm:h-[200px] sm:w-[500px]  ">
-					<TranslucidBox styles="text-white ">
-						<h1>Title</h1>
-						<p>
-							Entrée et sécurité : Sont strictement interdits à l'intérieur du festival : les objets
-							dangereux, les liquides, boissons, gourdes, bouteilles et autres contenants—même
-							vides—venant de l'extérieur.
-						</p>
+				<div class="space-y-3 sm:w-[70%]">
+					<TranslucidBox styles="text-white">
+						<div class="p-2">
+							<h1 class="sm:text-5xl mb-[10px]">Title</h1>
+							<p class="sm:text-4xl">
+								<span>Entrée et sécurité</span>: Sont strictement interdits à l'intérieur du
+								festival : les objets dangereux, les liquides, boissons, gourdes, bouteilles et
+								autres contenants—même vides—venant de l'extérieur.
+								<br />
+								<span>Jeune public</span>: L'entrée au festival est interdite aux personnes de moins
+								de 16 ans non-accompagnées d'un parent ou d'une personne adulte responsable.
+								<br />
+								<span>Attention</span>: toute sortie du site est définitive !
+							</p>
+						</div>
 					</TranslucidBox>
 
 					<TranslucidBox styles="text-white ">
-						<h1>Title</h1>
-						<p>
-							Entrée et sécurité : Sont strictement interdits à l'intérieur du festival : les objets
-							dangereux, les liquides, boissons, gourdes, bouteilles et autres contenants—même
-							vides—venant de l'extérieur.
+						<h1 class="sm:text-5xl mb-[10px]">Acces au Festival</h1>
+						<p class="sm:text-4xl">
+							<span>Venez en transports publics!</span>
+							<br />
+							Métro M1 : Arrêt EPFL.
+							<br />
+							Bus 31, 105 : Arrêt Blévallaire / Argand.
+							<br />
+							Bus 701 : Arrêt Pâqueret.
+							<br />
+							Des <span> navettes retou</span>des TL partiront de l'avenue Picard à<span
+								>01h30,02h30</span
+							>et<span>03h15</span>.Arrêts : Renens, Malley et Lausanne-Flon.
+							<br />
+							Attention : Le parking du Rolex Learning Center fermera ses portes à minuit.
 						</p>
 					</TranslucidBox>
 				</div>
 				<!--low priority info  -->
-				<div class=" space-y-3">
-					<TranslucidBox styles="text-white sm:h-[150px] ">
-						<h1>Title</h1>
-						<p>19h00: Ouverture des portes</p>
-						<p>19h00: Ouverture des portes</p>
-						<p>19h00: Ouverture des portes</p>
-						<p>19h00: Ouverture des portes</p>
+				<div class=" space-y-3 sm:h-[300px]">
+					<TranslucidBox styles="text-white ">
+						<h1 class="lg:text-[40px] sm:text-[40px] mb-[10px]">Title</h1>
 					</TranslucidBox>
 
-					<TranslucidBox styles="text-white sm:h-[150px]">
+					<TranslucidBox styles="text-white">
 						<h1>Acces au Festival</h1>
-						<p>19h00: Ouverture des portes</p>
-						<p>19h00: Ouverture des portes</p>
-						<p>19h00: Ouverture des portes</p>
-						<p>19h00: Ouverture des portes</p>
 					</TranslucidBox>
 				</div>
 			</div>
